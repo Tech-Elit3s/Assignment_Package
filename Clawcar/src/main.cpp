6df842452b822c5a341244223c9d5424ed62c84a
joshuanits <joshuanits@gmail.com>
2021-05-02 15:06:41 +0930

    Added base reading value

--- Clawcar/src/main.cpp
+++ Clawcar/src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
+    double* values = readValues();
 }
+
+void controlArm(double* values) {}
+void controlCar(double* values) {}
+
+double* readValues() {
+    double channelValues[5];
+
+    for (int i = 0; i < 5; i++) {
+        channelValues[i] = readValue(9 + i);
+    }
+
+    double finalValues[5];
+
+    // map value and filter deadzone
+    for (int i = 0; i < 5; i++) {
+        finalValues[i] = channelValues[i];
+    }
+
+    return finalValues;
+}
+
+double readValue(int pin) { return analogRead(pin); }
