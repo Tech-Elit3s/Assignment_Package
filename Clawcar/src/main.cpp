234d72549e45c1f5df096967e89e949404ca0dd4
S3858033 <61782232+S3858033@users.noreply.github.com>
2021-05-04 21:49:11 +1000

    Fixed values()

--- Clawcar/src/main.cpp
+++ Clawcar/src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 
 void setup() {
-<<<<<<< HEAD
   pinMode(9, INPUT);  // radio receiver channel 1 
   pinMode(10, INPUT); // radio receiver channel 2
   pinMode(11, INPUT); // radio receiver channel 3 
@@ -23,34 +22,23 @@ void loop() {
   Serial.print(values[3]);
   Serial.print(" - ");
   Serial.println(values[4]);
-=======
-    // put your setup code here, to run once:
-}
-
-void loop() {
-    // put your main code here, to run repeatedly:
-    double* values = readValues();
->>>>>>> 6df842452b822c5a341244223c9d5424ed62c84a
+  delete[] values;
 }
 
 void controlArm(double* values) {}
 void controlCar(double* values) {}
 
 double* readValues() {
-    double channelValues[5];
-
+    double *channelValues = new double[5];
+ 
     for (int i = 0; i < 5; i++) {
         channelValues[i] = readValue(9 + i);
     }
-
-    double finalValues[5];
-
-    // map value and filter deadzone
-    for (int i = 0; i < 5; i++) {
-        finalValues[i] = channelValues[i];
-    }
-
-    return finalValues;
+ 
+    return channelValues;
 }
 
-double readValue(int pin) { return analogRead(pin); }
+double readValue(int pin) { 
+    return pulseIn(pin, HIGH);
+}
+    // return analogRead(pin); }
