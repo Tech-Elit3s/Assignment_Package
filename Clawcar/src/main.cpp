748e0253179394795fbc994d20b34fcba16ca47c
S3858033 <61782232+S3858033@users.noreply.github.com>
2021-05-12 23:06:38 +1000

    Changes from 12/5 meeting + added esc pins to match wiring diagram

--- Clawcar/src/main.cpp
+++ Clawcar/src/main.cpp
@@ -1,100 +1,152 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+#define ESC_ARM_SIGNAL 1000
+#define ESC_ARM_TIME 2000
+
+const double driveSpeed = 1;
+const double turnSpeed = 1;
+
 int position1;
 Servo servo1;
 
+Servo topLeftESC;
+Servo topRightESC;
+Servo bottomLeftESC;
+Servo bottomRightESC;
+
+int channelMin[5] = {990, 985, 1000, 1020, 990};
+
+int channelMax[5] = {1975, 1965, 1990, 1990, 2000};
 
 void setup() {
+  pinMode(9, INPUT);
+  pinMode(10, INPUT);
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
+  pinMode(13, INPUT);
 
-// declares radio receiver channel pins
-  pinMode(9, INPUT);   
-  pinMode(10, INPUT); 
-  pinMode(11, INPUT);  
-  pinMode(12, INPUT); 
-  pinMode(13, INPUT); 
-  
-// Attaches the servo mounted to the lid to pin and 
-// positions it at the start up position. 
+  // Attaches the servo mounted to the lid to pin and
+  // positions it at the start up position.
   servo1.attach(3);
   position1 = 90;
   servo1.write(position1);
   delay(15);
 
- Serial.begin(9600);  
+  setupCar();
+
+  delay(ESC_ARM_TIME);
+
+  Serial.begin(9600);
+}
+
+// TODO: set correct pins
+// Attaches pin and set to middle
+void setupCar() {
+  topLeftESC.attach(7);
+  topRightESC.attach(8);
+  bottomLeftESC.attach(6);
+  bottomRightESC.attach(5);
+
+  topLeftESC.writeMicroseconds(ESC_ARM_SIGNAL);
+  topRightESC.writeMicroseconds(ESC_ARM_SIGNAL);
+  bottomLeftESC.writeMicroseconds(ESC_ARM_SIGNAL);
+  bottomRightESC.writeMicroseconds(ESC_ARM_SIGNAL);
 }
 
 void loop() {
-   // Reads radio input signals assigns values 
-   // to variable
-    double * values = readValues();
+  // Reads radio input signals assigns values
+  // to variable
+  int* values = readValues();
 
-// Prints out radio signals to terminal
-//       Serial.print(values[0]);
-//   Serial.print(" - ");
-//   Serial.print(values[1]);
-//   Serial.print(" - ");
-//   Serial.print(values[2]);
-//   Serial.print(" - ");
-//   Serial.print(values[3]);
-//   Serial.print(" - ");
-//   Serial.println(values[4]);
+  // Prints out radio signals to terminal
+         Serial.print(values[0]);
+     Serial.print(" - ");
+     Serial.print(values[1]);
+     Serial.print(" - ");
+     Serial.print(values[2]);
+     Serial.print(" - ");
+     Serial.print(values[3]);
+     Serial.print(" - ");
+     Serial.println(values[4]);
 
-// If the channel 5 switch is activated
-// the robot arm is being robot arm 
-// functionality is turned on.
-if(values[4] > 1500){
-    
+  // If the channel 5 switch is activated
+  // the robot arm is being robot arm
+  // functionality is turned on.
+  if (values[4] > 50) {
     controlArm(values);
-  } 
-  // Else the robot car functionality
-  // is turned on 
-//   else{
-// controlCar(values);
-//   }
+  } else {
+    controlCar(values);
+  }
+
   delete[] values;
 }
 
-// Robot Arm functionality 
-void controlArm(double* values) {
+// Robot Arm functionality
+void controlArm(int* values) {
+  // If the channel 2 joystick is moved backwards
+  // move the servo motor.
+  if (values[1] < 1700) {
+    position1 += 5;
 
-// If the channel 2 joystick is moved backwards 
-// move the servo motor. 
-if (values[1] < 1700){
-position1 += 5;
+    // Servo end point reached.
+    if (position1 > 120) {
+      position1 = 120;
+    }
 
-// Servo end point reached.
-if (position1 > 120){
-    position1 = 120;
-}
-    servo1.write(position1);              
-    delay(15);  
-}
-// If the channel 2 joystick is moved fowards 
-// move the servo motor. 
-if (values[1] > 1200){
+    servo1.write(position1);
+    delay(15);
+  }
+
+  // If the channel 2 joystick is moved fowards
+  // move the servo motor.
+  if (values[1] > 1200) {
     position1 -= 5;
     // Servo end point reached.
-    if (position1 < 20){
-    position1 = 20;
-}
-    servo1.write(position1);              
-    delay(15);  
-}
-}
-void controlCar(double* values) {}
-
-double* readValues() {
-    double *channelValues = new double[5];
- 
-    for (int i = 0; i < 5; i++) {
-        channelValues[i] = readValue(9 + i);
+    if (position1 < 20) {
+      position1 = 20;
     }
- 
-    return channelValues;
+
+    servo1.write(position1);
+    delay(15);
+  }
 }
 
-double readValue(int pin) { 
-    return pulseIn(pin, HIGH);
+// TODO: Handle range of values correctly for input
+// Select correct values
+void controlCar(int* values) {
+  int turnValue = values[0] - 50;
+  int forwardValue = values[1] - 50;
+
+  int leftSideSpeed = 0;
+  int rightSideSpeed = 0;
+
+  // Base speed for both sides is just the drive speed multiplied by forward
+  // value
+  leftSideSpeed = forwardValue * driveSpeed;
+  rightSideSpeed = forwardValue * driveSpeed;
+
+  leftSideSpeed += turnValue * turnSpeed;
+  rightSideSpeed -= turnValue * turnSpeed;
+
+  // Write the speed values to the ESCs. 90 is added as this is the 'center'
+  // point
+  topLeftESC.write(90 + leftSideSpeed);
+  bottomLeftESC.write(90 + leftSideSpeed);
+  topRightESC.write(90 + rightSideSpeed);
+  bottomRightESC.write(90 + rightSideSpeed);
 }
-    // return analogRead(pin); }  
+
+int* readValues() {
+  int* channelValues = new int[5];
+
+  for (int i = 0; i < 5; i++) {
+    double value = readValue(9 + i);
+    channelValues[i] =
+        (int)round(map(value, channelMin[i], channelMax[i], 0, 100));
+  }
+
+  return channelValues;
+}
+
+double readValue(int pin) { return pulseIn(pin, HIGH); }
